d0ab6cd99378456941ca954bcfe1e942fc03d60a
Luke Lai <llai@c1r1s7.42london.com>
2024-01-05 22:36:54 +0000

    Fix tabwidth

--- src/path.c
+++ src/path.c
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 19:35:32 by llai              #+#    #+#             */
-/*   Updated: 2024/01/05 20:49:51 by llai             ###   ########.fr       */
+/*   Updated: 2024/01/05 21:52:26 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdbool.h>
 
 // Checking the boundaries
-bool    is_safe(int i, int j, int rows, int cols)
+bool	is_safe(int i, int j, int rows, int cols)
 {
     if (i >= 0 && i < rows && j >= 0 && j < cols)
         return (true);
@@ -25,30 +25,25 @@ bool    is_safe(int i, int j, int rows, int cols)
 // Returns true if there is a valid path.
 bool    is_valid_path(char **map, int i, int j, bool **visited, int rows, int cols)
 {
+    bool    up;
+    bool    left;
+    bool    down;
+    bool    right;
     if (is_safe(i, j, rows, cols) && map[i][j] != '1' && !visited[i][j])
     {
         visited[i][j] = true;
-
         if (map[i][j] == 'E')
             return (true);
-
-        bool up = is_valid_path(map, i - 1, j, visited, rows, cols);
-
+        up = is_valid_path(map, i - 1, j, visited, rows, cols);
         if (up)
             return (true);
-
-        bool left = is_valid_path(map, i, j - 1, visited, rows, cols);
-
+        left = is_valid_path(map, i, j - 1, visited, rows, cols);
         if (left)
             return (true);
-
-        bool down = is_valid_path(map, i + 1, j, visited, rows, cols);
-
+        down = is_valid_path(map, i + 1, j, visited, rows, cols);
         if (down)
             return (true);
-
-        bool right = is_valid_path(map, i, j + 1, visited, rows, cols);
-
+        right = is_valid_path(map, i, j + 1, visited, rows, cols);
         if (right)
             return (true);
     }
@@ -57,21 +52,20 @@ bool    is_valid_path(char **map, int i, int j, bool **visited, int rows, int co
 
 bool    check_path(t_game *game)
 {
-    int rows;
-    int cols;
     int i;
     int j;
     int k;
     char    **map;
+    bool    **visited;
+    bool    flag;
 
     i = 0;
     j = 0;
     k = 0;
-    rows = game->height;
-    cols = game->width;
-    map = (char **)malloc(rows * sizeof(char *));
-    for (int i = 0; i < rows; ++i)
-        map[i] = (char *)malloc(cols * sizeof(char));
+    map = (char **)malloc(game->height * sizeof(char *));
+    while (i < game->height)
+        map[i++] = (char *)malloc(game->width * sizeof(char));
+    i = 0;
     while (game->map[i] != '\0')
     {
         map[j][k] = game->map[i];
@@ -83,31 +77,36 @@ bool    check_path(t_game *game)
             k = 0;
         }
     }
-    bool    **visited = (bool **)malloc(rows * sizeof(bool *));
-    for (int i = 0; i < rows; ++i)
+    visited = (bool **)malloc(game->height * sizeof(bool *));
+    i = 0;
+    while (i < game->height)
     {
-        visited[i] = (bool *)malloc(cols * sizeof(bool));
-        ft_memset(visited[i], 0, sizeof(bool) * cols);
+        visited[i] = (bool *)malloc(game->width * sizeof(bool));
+        ft_memset(visited[i], 0, sizeof(bool) * game->width);
+        i++;
     }
-
-    bool    flag = false;
-    for (int i = 0; i < rows; ++i)
+    flag = false;
+    i = 0;
+    while (i < game->height)
     {
-        for (int j = 0; j < cols; ++j)
+        j = 0;
+        while (j < game->width)
         {
             if (map[i][j] == 'P' && !visited[i][j])
             {
-                if (is_valid_path(map, i, j, visited, rows, cols))
+                if (is_valid_path(map, i, j, visited, game->height, game->width))
                 {
                     flag = true;
                     break ;
                 }
             }
+            j++;
         }
+        i++;
     }
-
-    for (int i = 0; i < rows; ++i)
-        free(visited[i]);
+    i = 0;
+    while (i < game->height)
+        free(visited[i++]);
     free(visited);
 
     if (flag)
@@ -121,19 +120,3 @@ bool    check_path(t_game *game)
         return (false);
     }
 }
-
-// int main(void)
-// {
-//     int rows = 5;
-//     int cols = 13;
-//     char    *map[] = {"1111111111111","100100C0000C1","1000011111001","1P0011E0000C1","1111111111111"};
-//     // char    **map = (char **)malloc(rows * sizeof(char *));
-//     // for (int i = 0; i < rows; ++i)
-//     //     map[i] = (char *)malloc(cols * sizeof(char));
-//         // {"1111111111111","100100C0000C1","1000011111001","1P0011E0000C1","1111111111111"};
-//
-//     isPath(map, rows, cols);
-//
-//     return (0);
-// }
-//
