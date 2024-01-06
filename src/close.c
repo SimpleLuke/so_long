5f8562ecfdd314ca67e78a72a4fb36be429be291
Luke Lai <llai@c1r1s7.42london.com>
2024-01-06 14:47:04 +0000

    Norm close.c

--- src/close.c
+++ src/close.c
@@ -6,39 +6,42 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 19:10:30 by llai              #+#    #+#             */
-/*   Updated: 2024/01/04 14:38:00 by llai             ###   ########.fr       */
+/*   Updated: 2024/01/06 14:46:50 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void    free_texture(t_game *game)
+void	free_texture(t_game *game);
+int		destory_game(t_game *game);
+int		esc_close_win(int keycode, t_game *vars);
+int		cross_close_win(t_game *vars);
+
+void	free_texture(t_game *game)
 {
-    mlx_destroy_image(game->mlx, game->texture.ground);
+	mlx_destroy_image(game->mlx, game->texture.space);
 }
 
-int destory_game(t_game *game)
+int	destory_game(t_game *game)
 {
-    free_texture(game);
-    mlx_destroy_window(game->mlx, game->win);
-    free(game->mlx);
-    exit(EXIT_SUCCESS);
-    return (0);
+	free_texture(game);
+	mlx_destroy_window(game->mlx, game->win);
+	free(game->mlx);
+	exit(EXIT_SUCCESS);
 }
 
-int esc_close_win(int keycode, t_game *vars)
+int	esc_close_win(int keycode, t_game *vars)
 {
-
-    if (keycode == ESC_KEY)
-    {
-        mlx_destroy_window(vars->mlx, vars->win);
-        exit(EXIT_SUCCESS);
-    }
-    return (0);
+	if (keycode == ESC_KEY)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(EXIT_SUCCESS);
+	}
+	return (0);
 }
 
-int cross_close_win(t_game *vars)
+int	cross_close_win(t_game *vars)
 {
-    mlx_destroy_window(vars->mlx, vars->win);
-    exit(EXIT_SUCCESS);
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit(EXIT_SUCCESS);
 }
