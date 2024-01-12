4ca08e25af538df3090a13341d0c306a88261406
Luke Lai <llai@c1r2s7.42london.com>
2024-01-12 12:04:14 +0000

    Fix norm

--- src/movement_utils.c
+++ src/movement_utils.c
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 20:52:58 by llai              #+#    #+#             */
-/*   Updated: 2024/01/10 12:08:02 by llai             ###   ########.fr       */
+/*   Updated: 2024/01/12 12:04:01 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,22 @@ void	move_up(t_game *game)
 {
 	game->player.location.y -= 1;
 	game->player.position = UP;
-	// put_img_to_img(game->base_image, game->player.sprite, game->player.location.x * 32, game->player.location.y * 32);
-	// mlx_put_image_to_window(game->mlx, game->win,
-	// 	game->player.sprite.img_ptr, game->player.location.x * 32,
-	// 	game->player.location.y * 32);
 }
 
 void	move_down(t_game *game)
 {
 	game->player.location.y += 1;
 	game->player.position = DOWN;
-	// put_img_to_img(game->base_image, game->player.sprite, game->player.location.x * 32, game->player.location.y * 32);
-	// mlx_put_image_to_window(game->mlx, game->win,
-	// 	game->player.sprite.img_ptr, game->player.location.x * 32,
-	// 	game->player.location.y * 32);
 }
 
 void	move_left(t_game *game)
 {
 	game->player.location.x -= 1;
 	game->player.position = LEFT;
-	// put_img_to_img(game->base_image, game->player.sprite, game->player.location.x * 32, game->player.location.y * 32);
-	// mlx_put_image_to_window(game->mlx, game->win,
-	// 	game->player.sprite.img_ptr, game->player.location.x * 32,
-	// 	game->player.location.y * 32);
 }
 
 void	move_right(t_game *game)
 {
 	game->player.location.x += 1;
 	game->player.position = RIGHT;
-	// put_img_to_img(game->base_image, game->player.sprite, game->player.location.x * 32, game->player.location.y * 32);
-	// mlx_put_image_to_window(game->mlx, game->win,
-	// 	game->player.sprite.img_ptr, game->player.location.x * 32,
-	// 	game->player.location.y * 32);
 }
