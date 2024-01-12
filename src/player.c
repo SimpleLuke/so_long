3e3864f6a748b12cc259608193453d2db1917284
Luke Lai <llai@c1r2s7.42london.com>
2024-01-12 17:01:03 +0000

    Fix norm

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 16:25:56 by llai              #+#    #+#             */
-/*   Updated: 2024/01/09 11:28:55 by llai             ###   ########.fr       */
+/*   Updated: 2024/01/12 17:00:45 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@ void	load_player_image(t_game *game);
 
 void	render_player(t_game *game)
 {
-	put_img_to_img(game->base_image, game->player.sprite, game->player.location.x * 32, game->player.location.y * 32);
-	// mlx_put_image_to_window(game->mlx, game->win,
-	// 	game->player.sprite.img_ptr, game->player.location.x * 32,
-	// 	game->player.location.y * 32);
+	put_img_to_img(game->base_image, game->player.sprite,
+		game->player.location.x * 32, game->player.location.y * 32);
 }
 
 void	load_player_image(t_game *game)
 {
-	game->player.sprite = new_file_img("assets/player_still.xpm", game->base_image.win);
+	game->player.sprite = new_file_img("assets/player_still.xpm",
+			game->base_image.win);
 }
