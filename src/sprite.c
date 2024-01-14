cb068fe999cca615cd120edd14ec97160954a1b1
Luke Lai <llai@c1r2s7.42london.com>
2024-01-14 13:27:09 +0000

    Add comment

--- src/sprite.c
+++ src/sprite.c
@@ -6,12 +6,35 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:24:07 by llai              #+#    #+#             */
-/*   Updated: 2024/01/13 18:30:16 by llai             ###   ########.fr       */
+/*   Updated: 2024/01/14 13:26:54 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** 
+ *  Summary of File:                                                          
+ *  
+ *  	This file contains code which creates sprites into slice and 
+ *  	converts it into frames, then it adds in the animation list.
+ *
+ * ************************************************************************** */
 #include "../includes/so_long.h"
 
+t_sprite	new_sprite(char *name, char *file_path, t_win *win);
+void		add_frame(t_animation *a, t_sprite s, t_sprite_slice slice);
+t_animation	*slice_sprite(t_sprite s, t_sprite_slice slice,
+						int frames, enum e_entity e);
+
+/* **************************************************************************
+ * t_sprite	new_sprite(char *name, char *file_path, t_win *win)
+ *
+ * Summary of the function:
+ * 
+ * This function creates new t_sprite from a xpm file.
+ *
+ * Parameters : A string of the sprite name, file path and t_win.
+ *
+ * Return Value : It returns a t_sprite struct.
+ * **************************************************************************/
 t_sprite	new_sprite(char *name, char *file_path, t_win *win)
 {
 	t_img	img;
@@ -21,34 +44,21 @@ t_sprite	new_sprite(char *name, char *file_path, t_win *win)
 		img, img.w, img.h, 0});
 }
 
-void	add_frame(t_animation *a, t_sprite s, t_sprite_slice slice)
-{
-	t_img	*frame;
-	int		i;
-	int		j;
-
-	// frame = (t_img *)ft_calloc(1, sizeof(t_img));
-	frame = (t_img *)malloc(sizeof(t_img));
-	if (!frame)
-		return ;
-	*frame = new_img(slice.width, slice.height, s.sprite_img.win);
-	i = 0;
-	while (i < slice.width)
-	{
-		j = 0;
-		while (j < slice.height)
-		{
-			put_pixel_img(*frame, j, i, get_pixel_img(s.sprite_img,
-					slice.x + j, slice.y + i));
-			j++;
-		}
-		i++;
-	}
-	ft_lstadd_back(&a->frames, ft_lstnew(frame));
-	// mlx_destroy_image(frame->win.mlx, frame->img_ptr);
-	// free(frame);
-}
-
+/* **************************************************************************
+ * t_animation	*slice_sprite(t_sprite s, t_sprite_slice slice,
+ *							int frames, enum e_entity e)
+ *
+ * Summary of the function:
+ * 
+ * This function creates a new t_animation, then it adds frames into
+ * the frames list in t_animation. Finally, it returns a t_animation
+ * for adding into the animation t_list in the t_sprite.
+ *
+ * Parameters : A t_sprite, t_sprite_slice, frames counts and 
+ * which entity.
+ *
+ * Return Value : It returns a pointer to t_animation.
+ * **************************************************************************/
 t_animation	*slice_sprite(t_sprite s, t_sprite_slice slice,
 						int frames, enum e_entity e)
 {
@@ -74,8 +84,39 @@ t_animation	*slice_sprite(t_sprite s, t_sprite_slice slice,
 	return (a);
 }
 
-void	destory_sprite(t_sprite s)
+/* **************************************************************************
+ * void	add_frame(t_animation *a, t_sprite s, t_sprite_slice slice)
+ *
+ * Summary of the function:
+ * 
+ * This function creates a new t_img from the sprite based 
+ * on the slice size. Then, it adds to animation frames list.
+ *
+ * Parameters : A pointer to t_animation, a t_sprite and t_sprite_slice.
+ *
+ * Return Value : It returns nothing.
+ * **************************************************************************/
+void	add_frame(t_animation *a, t_sprite s, t_sprite_slice slice)
 {
-	free(s.name);
-	free(s.file_path);
+	t_img	*frame;
+	int		i;
+	int		j;
+
+	frame = (t_img *)malloc(sizeof(t_img));
+	if (!frame)
+		return ;
+	*frame = new_img(slice.width, slice.height, s.sprite_img.win);
+	i = 0;
+	while (i < slice.width)
+	{
+		j = 0;
+		while (j < slice.height)
+		{
+			put_pixel_img(*frame, j, i, get_pixel_img(s.sprite_img,
+					slice.x + j, slice.y + i));
+			j++;
+		}
+		i++;
+	}
+	ft_lstadd_back(&a->frames, ft_lstnew(frame));
 }
